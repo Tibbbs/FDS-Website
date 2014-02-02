034ae750184c1b5c4714ce72cb709252fe601003
Matthew Thibodeau <matt.thibodeau@carolinadesigns.com>
2014-02-02 17:29:04 -0500

    Added Analytics tracking code and some minor changes

--- sponsors/2k13.php
+++ sponsors/2k13.php
@@ -210,11 +210,15 @@
 					timer_speed: 5000
 				}
 			});
+		</script>
+		<script>
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-			$(document).ready(function()
-			{
-				$(".equal").equalHeights();
-			});
+			ga('create', 'UA-47733608-1', 'firstflightrov.com');
+			ga('send', 'pageview');
 		</script>
 	</body>
 </html>
